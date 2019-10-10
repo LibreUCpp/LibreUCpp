0d20ecd43254fb2ff7948cf4846baca9ae2d994f
Hubert Denkmair <Hubert Denkmair>
2019-10-11 00:07:28 +0200

    STM32F0: Add UART Example

--- Examples/UART/UART.cpp
+++ Examples/UART/UART.cpp
@@ -22,13 +22,32 @@ extern "C" int _write(int, const void *buf, size_t count)
 int main()
 {
     Clock clk;
+#ifdef STM32F0308DISCOVERY
+    clk.Configure(ClockConfig::Get48MHzFrom8MHzCrystal());
+
+    Pin txd { Port::A[9] };
+    txd.EnablePeripheral();
+    //txd.SetDirection(Pin::Direction::OUTPUT);
+    txd.ConfigureMultiplex(Pin::Mux::AF1);
+
+    Pin rxd { Port::A[10] };
+    rxd.EnablePeripheral();
+    //rxd.SetDirection(Pin::Direction::INPUT);
+    rxd.ConfigureMultiplex(Pin::Mux::AF1);
+
+    uart.Setup(UART::INSTANCE::USART1, clk.CalcAHBFrequency());
+#endif
+#ifdef STM32F4DISCOVERY
+    clk.Configure(ClockConfig::Get168MHzFrom8MHzCrystal());
+#endif
+#ifdef SAMMY_C21
     clk.Configure(ClockConfig::Get48MHzFrom16MHzCrystal());
 
-    // TX pin Sammy C21: PB02 == SERCOM5/PAD[0]
+    // TX pin: PB02 == SERCOM5/PAD[0]
     Pin txd { Port::B[2] };
     txd.ConfigureMultiplex(Pin::Mux::D);
 
-    // RX pin Sammy C21: PB03 == SERCOM5/PAD[1]
+    // RX pin: PB03 == SERCOM5/PAD[1]
     Pin rxd { Port::B[3] };
     rxd.ConfigureMultiplex(Pin::Mux::D);
 
@@ -38,6 +57,7 @@ int main()
         UART::RXPAD::RXPO1, UART::TXPAD::TXPO0,
         GCLK::CalcFrequency(GCLK::GENERATOR::Generator_0, clk.GetXosc32kFrequency(), clk.GetXoscFrequency())
     );
+#endif
 
     uart.Init(115200, UART::BITS::EIGHT, UART::PARITY::NONE, UART::STOP_BITS::ONE);
     iprintf("Hello, World!\n");
